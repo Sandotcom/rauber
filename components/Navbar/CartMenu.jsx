d6a2713f49ac5f1e57b2894a952e8588a0618762
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-09-08 19:16:31 -0300

    Add Product component

--- components/Navbar/CartMenu.jsx
+++ components/Navbar/CartMenu.jsx
@@ -1,10 +1,20 @@
+import React, { useMemo } from 'react'
 import Link from 'next/link'
-import React from 'react'
 import { useSelector } from 'react-redux'
 import CartItem from './CartItem'
 
 const CartMenu = () => {
   const cart = useSelector(state => state.cart)
+  const text = useMemo(
+    () =>
+      cart
+        .reduce(
+          (message, product) => message.concat(`* ${product.name} - $${product.price}\n`),
+          ``
+        )
+        .concat(`\nTotal: $${cart.reduce((total, product) => total + product.price, 0)}`),
+    [cart]
+  )
 
   if(cart.length === 0){
     return (
@@ -23,22 +33,22 @@ const CartMenu = () => {
           <CartItem key={i} id={item.id} name={item.name} type={item.type} price={item.price} image={item.portada} />
         ))}
 
-      <div className='flex flex-col justify-between mt-6 space-y-1'>
-        <div className='flex flex-row justify-between text-lg font-semibold'>
-          <p>Total</p>
-          <p>$ 1800</p>
-        </div>
+        <div className='flex flex-col justify-between mt-6 space-y-1'>
+          <div className='flex flex-row justify-between text-lg font-semibold'>
+            <p>Total</p>
+            <p>$ {cart.reduce((total, product) => total + product.price, 0)}</p>
+          </div>
 
-        <div className='flex flex-row justify-between pt-2 space-x-1 text-sm'>
-          <Link href='/carrito'>
-            <button className='h-10 px-5 font-semibold rounded-md border border-slate-800 text-slate-900'>Modificar carrito</button>
-          </Link>
-            
-          <Link href='/checkout'>
-            <button className='h-10 px-5 font-semibold rounded-md bg-green-700 text-white'>Finalizar pedido</button>
-          </Link>
+          <div className='flex flex-row justify-between pt-2 space-x-1 text-sm'>
+            <Link href='/carrito'>
+              <button className='h-10 px-5 font-semibold rounded-md border border-slate-800 text-slate-900'>Modificar carrito</button>
+            </Link>
+              
+            <Link href={`https://wa.me/542214205694?text=${encodeURIComponent(text)}`}>
+              <button className='h-10 px-5 font-semibold rounded-md bg-green-700 text-white'>Finalizar pedido</button>
+            </Link>
+          </div>
         </div>
-      </div>
       </div>
     )
   }
---
+++ components/Product.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { ADD_ITEM } from '../app/actions/actionTypes'
+import Link from 'next/link'
+import { useDispatch } from 'react-redux'
+
+const Product = ({product}) => {
+  const { id, name, type, description, price, portada } = product
+  const dispatch = useDispatch()
+
+  const onClick = (e, product) => {
+    e.preventDefault()
+    dispatch({ type: ADD_ITEM, payload: product })
+  }
+
+  return (
+    <div className='flex font-poppins rounded-md shadow bg-gray-200'>
+      <div className='relative'>
+        <img src={portada} alt={name} className='w-full h-full object-cover rounded-l-md' />
+      </div>
+      <div className='flex-auto p-3 border-slate-900'>
+        <div className='flex justify-between'>
+          <div>
+            <h4 className={name.length > 12 ? 'flex-auto text-base font-semibold text-slate-900' : 'flex-auto text-lg font-semibold text-slate-900'}>{name}</h4>
+            <p className='flex-none text-sm font-medium text-slate-700 mt-2'>{type}</p>
+          </div>
+          <p className='text-lg font-semibold text-slate-500'>${price}</p>
+        </div>
+                
+        <div className="flex mt-3 text-sm">
+          <div className="flex-auto flex space-x-4">
+            <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type='button' onClick={(e) => onClick(e, product)}>
+              Agregar
+            </button>
+            <Link key={id} href={`cervezas/${id}`}>
+              <button className="h-10 px-6 font-semibold rounded-md border border-slate-800 text-slate-900" type="button">
+                Detalle
+              </button>
+            </Link>
+          </div>                  
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Product
