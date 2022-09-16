f772ff8b2a6c6efdb3080f8d79c9d6a1767ca8e3
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-09-16 11:58:48 -0300

    Fix image?

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className='flex flex-col p-4 space-y-2 md:max-w-screen-xl'>
         <div className='flex flex-col md:grid md:grid-cols-2'>
           <div className='md:px-20'>
-            <img className='w-auto h-auto rounded-xl' src='/RAUBER1.jpg' />
+            <img className='w-auto h-auto rounded-xl' src='/rauber1.jpg' />
           </div>
           <div className='flex flex-col space-y-2 pt-4'>
             <h2 className='font-semibold text-xl'>¿Quiénes somos?</h2>
