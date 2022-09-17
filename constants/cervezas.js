5cfa7cbdf661138e71024f5f66f269a92a2fb207
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-09-17 12:04:11 -0300

    Update prices & add images

--- constants/cervezas.js
+++ constants/cervezas.js
@@ -1,11 +1,13 @@
 const cervezas = [
   // { id: 'golden-ale', name: 'Golden Ale', description: 'Nuestra Golden Ale...', image: null },
-  { id: 'session-ipa', name: 'Session IPA', type: '(Lata 473ml)', description: 'Nuestra Session IPA...', price: 350, portada: '/cervezas/session/session1.jpg', image: ['/cervezas/session/session1.jpg', '/cervezas/session/session2.jpg', '/cervezas/session/sessionInfo.jpg'] },
-  { id: 'ipa-bandida', name: 'IPA Bandida', type: '(Lata 473ml)', description: 'La mejor de las nuestras', price: 380, portada: '/cervezas/bandida/bandida1.jpg', image: ['/cervezas/bandida/bandida1.jpg', '/cervezas/bandida/bandida2.jpg', '/cervezas/bandida/bandidaInfo.jpg'] },
-  { id: 'australian-ipa', name: 'DDH Australian IPA', type: '(Lata 473ml)', description: 'Nuestra Australian IPA...', price: 370, portada: '/cervezas/australian/aust1.jpg', image: ['/cervezas/australian/aust1.jpg', '/cervezas/australian/aust2.jpg', '/cervezas/australian/austInfo.jpg'] },
-  { id: 'sour', name: 'Qué Bacán', type: '(Lata 473ml)', description: 'Nuestra Sour...', price: 420, portada: '/cervezas/sour/sour1.png', image: ['/cervezas/sour/sour1.png', '/cervezas/sour/sour2.jpg'] },
-  { id: 'red-ale', name: 'Red Ale', type: '(Lata 473ml)', description: 'Nuestra Red Ale...', price: 300, portada: '/cervezas/red/red1.png', image: ['/cervezas/red/red1.jpg', '/cervezas/red/red2.jpg'] },
-  { id: 'stout', name: 'Stout', type: '(Lata 473ml)', description: 'Nuestra Stout...', price: 320, portada: '/cervezas/stout/stout1.jpg', image: ['/cervezas/stout/stout1.jpg', '/cervezas/stout/stout1.jpg'] },
+  { id: 'session-ipa', name: 'Session IPA', type: '(Lata 473ml)', description: 'Nuestra Session IPA...', price: 330, portada: '/cervezas/session/session1.jpg', image: ['/cervezas/session/session1.jpg', '/cervezas/session/session2.jpg', '/cervezas/session/sessionInfo.jpg'] },
+  { id: 'ipa-bandida', name: 'IPA Bandida', type: '(Lata 473ml)', description: 'La mejor de las nuestras', price: 370, portada: '/cervezas/bandida/bandida1.jpg', image: ['/cervezas/bandida/bandida1.jpg', '/cervezas/bandida/bandida2.jpg', '/cervezas/bandida/bandidaInfo.jpg'] },
+  { id: 'australian-ipa', name: 'DDH Australian IPA', type: '(Lata 355ml)', description: 'Nuestra Australian IPA...', price: 360, portada: '/cervezas/australian/aust1.jpg', image: ['/cervezas/australian/aust1.jpg', '/cervezas/australian/aust2.jpg', '/cervezas/australian/austInfo.jpg'] },
+  { id: 'neipa', name: 'Vidas Comunes', type: '(Lata 473ml)', description: 'Neipa', price: 470, portada: '/cervezas/neipa/neipa1.jpg', image: ['/cervezas/neipa/neipa1.jpg', '/cervezas/neipa/neipa1.jpg'] },
+  { id: 'sour', name: 'Qué Bacán', type: '(Lata 473ml)', description: 'Nuestra Sour...', price: 430, portada: '/cervezas/sour/sour1.png', image: ['/cervezas/sour/sour1.png', '/cervezas/sour/sour2.jpg'] },
+  { id: 'franieri', name: 'Frañeri', type: '(Lata 473ml)', description: 'Nuestra Franieri...', price: 300, portada: '/cervezas/franieri/franieri2.jpg', image: ['/cervezas/franieri/franieri1.jpg', '/cervezas/franieri/franieri2.jpg'] },
+  { id: 'red-ale', name: 'Red Ale', type: '(Lata 473ml)', description: 'Nuestra Red Ale...', price: 250, portada: '/cervezas/red/red1.png', image: ['/cervezas/red/red1.jpg', '/cervezas/red/red2.jpg'] },
+  { id: 'stout', name: 'Stout', type: '(Lata 473ml)', description: 'Nuestra Stout...', price: 280, portada: '/cervezas/stout/stout1.jpg', image: ['/cervezas/stout/stout1.jpg', '/cervezas/stout/stout1.jpg'] },
 ]
 
 export default cervezas
