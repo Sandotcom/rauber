6ecf4b81beed016bea9f3d63a979e31c8e445529
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-09-22 14:23:16 -0300

    Add getStaticProps to product & link to /id

--- pages/productos.js
+++ pages/productos.js
@@ -1,20 +1,28 @@
 import PageLayout from "../components/PageLayout"
-import cervezas from '../constants/cervezas'
 import Product from "../components/Product"
+import cervezas from '../constants/cervezas'
 
-export default function Tienda() {
+export default function Tienda({ data }) {
   return (
     <PageLayout title="Räuber - Tienda Online">
       <div className="flex flex-col p-4 space-y-2 md:max-w-screen-xl">
         <h2 className='font-semibold text-xl py-4'>Nuestros productos</h2>
         <div className='flex flex-col gap-8'>
           <div className='flex flex-col md:grid md:grid-cols-2 gap-4'>
-            {cervezas.map((product, i) => (         
-                <Product key={i} product={product} />
-            ))}
+            {data.map((product, i) => (     
+              <Product key={i} product={product} />
+              ))}
           </div>
         </div>
       </div>
     </PageLayout>
   )
+}
+
+export async function getStaticProps(){
+  const data = cervezas
+
+  return {
+    props: { data }
+  }
 }
